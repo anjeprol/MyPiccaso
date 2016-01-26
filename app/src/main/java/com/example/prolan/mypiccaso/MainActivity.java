5f8aab2d8bd2a11422ac68ee29f67d98bcf718b5
anjeprol <anjeprol_prado@hotmail.com>
2016-01-26 15:58:47 -0500

    comment

---
+++ app/src/main/java/com/example/prolan/mypiccaso/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.prolan.mypiccaso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loadImg(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("https://i.ytimg.com/vi/pzPxhaYQQK8/maxresdefault.jpg")
+                .into(imageView);
+    }
+}
